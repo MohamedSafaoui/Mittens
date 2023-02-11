e4c5f6b9efb992a11d841ee25b8db21e012ee6c8
Mohamed Safaoui <Mohamedsafaoui21@gmail.com>
2023-02-11 15:09:52 -0500

    Initial Commit

---
+++ Mittens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mittens
+//
+//  Created by MohamedSafaoui on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mittens/MittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MittensApp.swift
+//  Mittens
+//
+//  Created by MohamedSafaoui on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
